5e3cb43dd3e69f096de2c248f5d0a9da78c33254
chunfu <hideoryan@gmail.com>
2020-07-01 22:28:31 +0800

    add header & footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Box, Text, Divider } from 'gestalt';
 import axios from 'axios';
 import { get } from 'lodash';
 import RetailerGame from './components/RetailerGame';
@@ -46,10 +47,35 @@ const App = (props) => {
 
   return (
     <IndexContext.Provider value={{ ...demandsApiData, pickDemandsAgain }}>
+      <Box
+        color="blue"
+        height={60}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        marginBottom={2}
+      >
+        <Text align="center" color="white" size="lg" weight="bold">
+          Retailer Game
+        </Text>
+      </Box>
       <Router>
         <Route exact path="/" component={RetailerGame} />
         <Route exact path="/upload" component={Upload} />
       </Router>
+      <Box marginBottom={10} />
+      <Divider />
+      <Box
+        height={60}
+        display="flex"
+        alignItems="center"
+        justifyContent="end"
+        padding={8}
+      >
+        <Text align="left" size="lg" weight="bold">
+          Made by the IEDO Lab, NTU IM
+        </Text>
+      </Box>
     </IndexContext.Provider>
   );
 };
