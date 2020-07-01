b295bf2151ad674276a0905d817968ee4024a844
chunfu <hideoryan@gmail.com>
2020-07-01 22:59:41 +0800

    update font color & favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -72,7 +72,7 @@ const App = (props) => {
         justifyContent="end"
         padding={8}
       >
-        <Text align="left" size="sm" weight="bold" color="lightGray">
+        <Text align="left" size="sm" weight="bold" color="gray">
           Made by the IEDO Lab, NTU IM
         </Text>
       </Box>
